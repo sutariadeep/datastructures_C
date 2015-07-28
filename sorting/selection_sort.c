9b9fccefb44502782f4a1edc29f7561d3713d373
sutariadeep <sutariadeep@gmail.com>
2015-07-27 23:57:16 -0700

    Adding bubble_sort

--- sorting/selection_sort.c
+++ sorting/selection_sort.c
@@ -13,9 +13,7 @@ void selectionSort(int arr[], int n)
         min_idx = i;
         for (j = i+1; j < n; j++)
           if (arr[j] < arr[min_idx])
-            min_idx = j;
- 
-        // Swap the found minimum element with the first element
+        {
         int temp = arr[i];
         arr[i] = arr[min_idx];
         arr[min_idx] = temp;
