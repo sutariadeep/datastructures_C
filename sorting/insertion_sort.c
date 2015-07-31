552771889f72184c15222ab4cb08c9b816c25efb
sutariadeep <sutariadeep@gmail.com>
2015-07-30 18:21:45 -0700

    Update insertion_sort.c

--- sorting/insertion_sort.c
+++ sorting/insertion_sort.c
@@ -9,17 +9,17 @@ void insertionSort(int arr[], int n)
    for (i = 1; i < n; i++)
    {
        key = arr[i];
-       j = i-1;
+       j = i;
  
        /* Move elements of arr[0..i-1], that are
           greater than key, to one position ahead
           of their current position */
-       while (j >= 0 && arr[j] > key)
+       while (j > 0 && arr[j] > key)
        {
-           arr[j+1] = arr[j];
+           arr[j] = arr[j-1];
            j = j-1;
        }
-       arr[j+1] = key;
+       arr[j] = key;
    }
 }
  
