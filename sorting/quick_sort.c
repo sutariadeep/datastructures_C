fb9544a0dab0a5d39f14f55d921c1cfb537c8b10
sutariadeep <sutariadeep@gmail.com>
2015-07-31 17:12:06 -0700

    Update quick_sort.c

--- sorting/quick_sort.c
+++ sorting/quick_sort.c
@@ -1,95 +1,59 @@
-/* C program for merge sort */
-#include<stdlib.h>
-#include<stdio.h>
- 
-/* Function to merge the two haves arr[l..m] and arr[m+1..r] of array arr[] */
-void merge(int arr[], int l, int m, int r)
+/* A typical recursive implementation of quick sort */
+
+/* This function takes last element as pivot, places the pivot element at its
+   correct position in sorted array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right of pivot */
+
+void swap(int *xp, int *yp)
 {
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 =  r - m;
- 
-    /* create temp arrays */
-    int L[n1], R[n2];
- 
-    /* Copy data to temp arrays L[] and R[] */
-    for(i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for(j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
- 
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2)
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+int partition (int arr[], int l, int h)
+{
+    int x = arr[h];
+    int i = (l - 1);
+    int j;
+
+    for (j = l; j <= h- 1; j++)
     {
-        if (L[i] <= R[j])
+        if (arr[j] <= x)
         {
-            arr[k] = L[i];
             i++;
+            swap (&arr[i], &arr[j]);
         }
-        else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
     }
- 
-    /* Copy the remaining elements of L[], if there are any */
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
- 
-    /* Copy the remaining elements of R[], if there are any */
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+    swap (&arr[i + 1], &arr[h]);
+    return (i + 1);
 }
- 
-/* l is for left index and r is right index of the sub-array
-  of arr to be sorted */
-void mergeSort(int arr[], int l, int r)
+
+/* A[] --> Array to be sorted, l  --> Starting index, h  --> Ending index */
+void quickSort(int A[], int l, int h)
 {
-    if (l < r)
+    if (l < h)
     {
-        int m = l+(r-l)/2; //Same as (l+r)/2, but avoids overflow for large l and h
-        mergeSort(arr, l, m);
-        mergeSort(arr, m+1, r);
-        merge(arr, l, m, r);
+        int p = partition(A, l, h); /* Partitioning index */
+        quickSort(A, l, p - 1);
+        quickSort(A, p + 1, h);
     }
 }
- 
- 
-/* UITLITY FUNCTIONS */
-/* Function to print an array */
-void printArray(int A[], int size)
+
+// A utility function to print contents of arr
+void printArr( int arr[], int n )
 {
     int i;
-    for (i=0; i < size; i++)
-        printf("%d ", A[i]);
-    printf("\n");
+    for ( i = 0; i < n; ++i )
+        printf( "%d ", arr[i] );
 }
- 
-/* Driver program to test above functions */
+
+// Driver program to test above functions
 int main()
 {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int arr_size = sizeof(arr)/sizeof(arr[0]);
- 
-    printf("Given array is \n");
-    printArray(arr, arr_size);
- 
-    mergeSort(arr, 0, arr_size - 1);
- 
-    printf("\nSorted array is \n");
-    printArray(arr, arr_size);
+    int arr[] = {4, 3, 5, 2, 1, 3, 2, 3};
+    int n = sizeof( arr ) / sizeof( *arr );
+    quickSort( arr, 0, n - 1 );
+    printArr( arr, n );
     return 0;
 }
