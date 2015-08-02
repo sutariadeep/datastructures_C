9b725109623782c76e5e9a9f55567c4297c593eb
sutariadeep <sutariadeep@gmail.com>
2015-08-01 18:59:28 -0700

    Update bubble_sort.c

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -7,12 +7,15 @@ void bubbleSort(int arr[], int n)
 {
    int i, j;
    for (i = 0; i < n-1; i++)      
-       for (j = 0; j < n-i-1; j++) //Last i elements are already in place  
-           if (arr[j] > arr[j+1]){
+       for (j = 0; j < n-i-1; j++){ //Last i elements are already in place  
+           if (arr[j] > arr[j+1])
+           {
             int temp = arr[j];
             arr[j] = arr[j+1];
             arr[j+1] = temp;
           }
+       }
+   }
 }
  
 /* Function to print an array */
